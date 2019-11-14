a5b03d9720c9c67bc14d495618e5cdf85b43dcbe
Pro X <pro@proteanx.com>
2019-11-14 12:13:09 -0500

    2.1.0

--- bitcorn_autoinstall.sh
+++ bitcorn_autoinstall.sh
@@ -56,7 +56,7 @@ fi
 genkey=$1
 clear
 
-echo -e "${GREEN} ------- Bitcorn MASTERNODE INSTALLER v2.1.1--------+
+echo -e "${GREEN} ------- Bitcorn MASTERNODE INSTALLER v2.1.0--------+
  |                                                  |
  |                                                  |::
  |       The installation will install and run      |::
@@ -184,8 +184,8 @@ fi
 #Installing Daemon
 cd ~
 rm -rf /usr/local/bin/bitcorn*
-wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.1/BITCORN-2.1.1-daemon-ubuntu_16.04.tar.gz
-tar -xzvf BITCORN-2.1.1-daemon-ubuntu_16.04.tar.gz
+wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.0/BITCORN-2.1.0-daemon-ubuntu_16.04.tar.gz
+tar -xzvf BITCORN-2.1.0-daemon-ubuntu_16.04.tar.gz
 sudo chmod -R 755 bitcorn-cli
 sudo chmod -R 755 bitcornd
 cp -p -r bitcornd /usr/local/bin
@@ -228,7 +228,7 @@ done
     bitcorn-cli stop
     sleep 5
 cd ~/.bitcorn/ && rm -rf blocks chainstate sporks zerocoin
-cd ~/.bitcorn/ && wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.1/bootstrap.zip
+cd ~/.bitcorn/ && wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.0/bootstrap.zip
 cd ~/.bitcorn/ && unzip bootstrap.zip	
 # Create bitcorn.conf
 cat <<EOF > ~/.bitcorn/bitcorn.conf
