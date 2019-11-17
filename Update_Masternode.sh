4d2c420d9daa98c2d167645ec45aae173d921f98
Pro X <pro@proteanx.com>
2019-11-16 23:14:35 -0500

    v2.1.2

--- Update_Masternode.sh
+++ Update_Masternode.sh
@@ -4,10 +4,10 @@ echo "We will update your Bitcorn Masternode right now"
 sudo apt-get -y install unzip
 bitcorn-cli stop
 rm -rf /usr/local/bin/bitcorn*
-mkdir CORNUpdated_2.1.0
-cd CORNUpdated_2.1.0
-wget https://github.com/BITCORNProject/BITCORN/releases/download/2.1.0/BITCORN-2.1.0-daemon.tar.gz
-tar xzvf BITCORN-2.1.0-daemon.tar.gz
+mkdir CORNUpdated_v2.1.2
+cd CORNUpdated_v2.1.2
+wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.2/BITCORN-v2.1.2-daemon.tar.gz
+tar xzvf BITCORN-v2.1.2-daemon.tar.gz
 mv bitcornd /usr/local/bin/bitcornd
 mv bitcorn-cli /usr/local/bin/bitcorn-cli
 chmod +x /usr/local/bin/bitcorn*
@@ -15,11 +15,11 @@ rm -rf ~/.bitcorn/blocks
 rm -rf ~/.bitcorn/chainstate
 rm -rf ~/.bitcorn/peers.dat
 cd ~/.bitcorn/
-wget https://github.com/BITCORNProject/BITCORN/releases/download/2.1.0/bootstrap.zip
+wget https://github.com/BITCORNProject/BITCORN/releases/download/v2.1.2/bootstrap.zip
 unzip bootstrap.zip
 
 cd ..
-cd CORNUpdated_2.1.0
+cd CORNUpdated_v2.1.2
 bitcornd -daemon
 sleep 10
 echo "Masternode Updated!"
